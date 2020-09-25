a57e4d2d6e4bf16d306af72331c26481ca1af42c
Konstantinospil <kpilpilidis@gmail.com>
2020-09-26 00:04:05 +0200

    Started creating object weapons Implemented getter and setters

--- game.py
+++ game.py
@@ -41,7 +41,7 @@ class Hole:
     cycle: removes corpses, re-populates the prison and relocates the inmates
         :returns None
     """
-    def __init__(self,n=FLOORS,m=CELLS,t=TOTAL):
+    def __init__(self,n=FLOORS,m=CELLS):
         """
         Init method of a Hole object: populates the Hole with inmate's and calculates the calories needed for the platform.
         :param n: int indicating the number of floors.
@@ -50,22 +50,34 @@ class Hole:
         :return: None
         """
         n,m,t = [homebrew.notInt(x) for x in [n,m,t]]
-        self.provided_cal = 0                           # float: number of calories the platform carries
-        self.prison = {}                                # dict: holding the inmates objects
-        self.duration = t
+        self.__provided_cal = 0                           # float: number of calories the platform carries
+        self.__prison = {}                                # dict: holding the inmates objects
         id = 0
         for i in range(n):
-            self.prison[i] = {}
+            self.__prison[i] = {}
             for j in range(m):
                 id +=1
-                self.prison[i][j] = Inmate(id)
-                self.provided_cal += self.prison[i][j].curr_needs
+                self.__prison[i][j] = Inmate(id)
+                self.__provided_cal += self.__prison[i][j].curr_needs
+        logger.INFO(print(self))
     def __str__(self):
         """
         str method of a Hole object
         :return: str
         """
-        return "Floors: {} \nCell size: {} \nCalories on platform: {}".format(len(self.prison),len(self.prison[0]),self.provided_cal)
+        return "Object Hole - Floors: {} \nCell size: {} \nCalories on platform: {}".format(len(self.__prison),len(self.__prison[0]),self.__provided_cal)
+    def getter_provided_calories(self):
+        """
+        Method to get the number of calories the platform carries
+        :return: float
+        """
+        return self.__provided_cal
+    def setter_provided_calories(self):
+        """
+        Method to re-calculate the number of calories a platform should carry (ex. after new inmates are introduced)
+        :return: None
+        """
+        self.__provided_cal = sum({k: sum(v.values()) for k, v in self.__prison.items()}.values())
     def feed(self):
         """
         The method starts the platform full with food which is eaten along the way. When the food is totally eaten, it
@@ -73,18 +85,25 @@ class Hole:
         :method eat: The method that calls the necessary functions for the inmates to eat.
         :return: None
         """
-        def eat(self, loc, available):
+        def calc_consumption(self, loc, available):
             """
             The method calls the function that makes the inmates of a floor to start eating and if there is not enough
-            calls the function that makes them respond to the lack of food
+            calls the function that makes them respond to the lack of food.
             :param loc: The floor of the prison which is being fed
             :param available: The quantity of available food
             :return:
             """
-            claims = 0
+            claims = []
+            consumption = 0
             for i in range(len(self.prison[loc])):
-                claims += self.prison[loc][i].claim() * (self.prison[loc][i].alive == 1)
-            if claims > available:
+                ate = self.prison[loc][i].eat() * (self.prison[loc][i].alive == 1)
+                consumption += ate
+                claims.append(ate)
+            if consumption <= available:
+                for i in range(len(self.prison[loc])):
+                    if self.prison[loc][i].alive == 1:
+                        self.prison[loc][i].hunger() -= self.prison[loc][i].eat()
+            else:
                 response = {}
                 for i in range(len(self.prison[loc])):
                     if self.prison[loc][i].alive == 1:
@@ -93,12 +112,12 @@ class Hole:
         food = self.provided_cal
         for i in range(len(self.prison)):
             if food > 0:
-                food -= eat(i, food)
+                food -= calc_consumption(i, food)
             else:
                 for j in range(len(self.prison[i])):
                     if self.prison[i][j].alive == 1:
                         self.prison[i][j].hunger += self.prison[i][j].curr_needs
-    def cycle(self):
+    def organise(self):
         """
         The method removes dead inmates, relocates inmates from non-full floors together with other inmates from
         non-full floors. Generates inmates to fill the Hole. Relocates all inmates.
@@ -112,41 +131,96 @@ class Hole:
 
 class Inmate(object):
     """
-    An instance of an inmate.
+    Object inmate responds to the amount of food available on the platform and possesses an object.
     Methods:
     __init__: Generates a random gender, weight, height, age, BMI and number of calories the object needs per day.
         :returns None
     __str__: string method of a class Inmate
         :returns str
+    *getters: methods that return the biometric characteristics of an inmate (gender, height, weight) and
+        - These characteristics cannot be changed by the user.
     weight_change:
         :returns None
     """
-    def __init__(self,id):
+    def __init__(self,id,greed):
         """
         Init method of a Inmate object.
         Each inmate's caloric needs are calculated according to a random gender, age, weight, and height based on the
         Mifflin St Jeor Equation.
         Hunger is a variable holding the caloric deficit/surplus of the inmate.
+        :param greed: float between 1 and MAX_FOOD / curr_needs represents how much an inmate tries to overeat.
         """
-        self.id = id
-        self.man = randint(0, 1)
+        self.__id = id
+        self.__man = randint(0, 1)
         age = randint(b_age[0],b_age[1])
-        self.height = homebrew.randtrunc(b_hei[0],b_hei[1],AV_HEI_M * (self.man == 1) + AV_HEI_W * (self.man == 0),SD_HEI_B)
+        self.__height = homebrew.randtrunc(b_hei[0],b_hei[1],AV_HEI_M * (self.__man == 1) + AV_HEI_W * (self.__man == 0),SD_HEI_B)
         bmi = homebrew.randtrunc(b_bmi[0],b_bmi[1],AV_BMI,SD_BMI)
-        self.weight, self.min_weight = ((self.height/100) ** 2) * bmi, ((self.height/100) ** 2) * MIN_BMI
-        self.weight = round(self.weight, 2)
-        self.height = round(self.height, 2)
-        self.init_needs = round(FACTOR_W * self.weight + 6.25 * self.height - 5 * age + (self.man == 1) * 5 - 161 * (self.man == 0),2)
-        self.curr_needs = self.init_needs
+        self.__weight, self.__min_weight = ((self.__height/100) ** 2) * bmi, ((self.__height/100) ** 2) * MIN_BMI
+        self.__weight = round(self.__weight, 2)
+        self.__height = round(self.__height, 2)
+        self.__init_needs = round(FACTOR_W * self.__weight + 6.25 * self.__height - 5 * age + (self.__man == 1) * 5 - 161 * (self.__man == 0),2)
+        self.curr_needs = self.__init_needs
+        assert (type(greed) == float or type(float) == int) and \
+               1 <= greed <= MAX_FOOD / self.curr_needs, "arg greed did not have a valid value"
         self.hunger = 0
-        self.alive = 1
+        self.__alive = 1
+        logger.info(print(self))
     def __str__(self):
         """
         The string method of an object of Inmate type.
         :return: string
         """
-        return "{}: {} {} - W: {} H: {} / N: {}/{} ({})".format(self.id,'man  ' if self.man==1 else 'woman','alive' if self.alive == 1 else 'dead',
-                                                                round(self.weight,1),round(self.height,1),self.curr_needs,self.init_needs,self.hunger)
+        return "Object Inmate {} (Greed {}): {} {} - W: {} H: {} / N: {}/{} ({})".format(self.id,self.greed,
+                                            'man  ' if self.man==1 else 'woman','alive' if self.alive == 1 else 'dead',
+                                            round(self.weight,1),round(self.height,1),self.curr_needs,self.init_needs,
+                                            self.hunger)
+    def getter_id(self):
+        """
+        Method that returns the ID of an inmate
+        :return: int
+        """
+        return self.__id
+    def getter_gender(self):
+        """
+        Method that returns the gender of an inmate
+        :return: str
+        """
+        return 'man' if self.__man == 1 else 'woman'
+    def getter_height(self):
+        """
+        Method that returns the height of an inmate
+        :return: float
+        """
+        return self.__height
+    def getter_weight(self):
+        """
+        Method that returns the weight of an inmate
+        :return: float
+        """
+        return self.__weight
+    def getter_init_needs(self):
+        """
+        Method that returns the initial caloric needs of an inmate
+        :return: float
+        """
+        return self.__init_needs
+    def die(self):
+        """
+        Setter method for the state of an inmate (alive to dead)
+        :return: None
+        """
+        if self.__alive == 1:
+            self.alive == 0
+        else:
+            logger.warning("Just tries to kill a dead inmate {}".format(self.__id))
+    def equip(self,item):
+        """
+        Method gives an item of type Possession to an inmate
+        :param item: object Possession
+        :return: None
+        """
+        assert type(item) == Possession,"arg item has an invalid value"
+        self.possession = item
     def weight_change(self):
         """
         The method changes the BMI and the weight of an inmate and calculates whether he/she is dead.
@@ -162,3 +236,36 @@ class Inmate(object):
             self.bmi = self.weight / (self.height ** 2)
             if self.bmi < MIN_BMI:
                 self.alive = 0
+    def eat(self):
+        """
+        Method that returns the maximum that an inmate will try to eat.
+        :return: float
+        """
+        return self.greed * self.greed
+    def response(self):
+        """
+        The method contains the inmate's behavior after different events:
+        Events list:
+        1. Not enough food:
+            a. The other inmate(s) attacked
+            b. The other inmate(s) did not attack
+            c. After attack,
+        :return:
+        """
+class Possession(object):
+    """
+    Objects of the type Possessions are equipped by the inmates. Each inmate can possess one object and use any object
+    left by dead inmates to which he / she has access
+    Methods: __init__
+    """
+    def __init__(self,lethality=0,caloric_needs=0,caloric_value=0):
+        self.__lethality = lethality
+        self.__caloric_needs = caloric_needs
+        self.__caloric_value = caloric_value
+
+class Weapon(Possession):
+    """
+    Objects of the subclass Weapon are Possessions
+    """
+    def __init__(self,lethality,caloric_needs=0,caloric_value=0):
+        super.__init__(lethality,caloric_needs,caloric_value)
